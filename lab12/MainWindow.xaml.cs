d2fd70f5534faae3d848994e553debe02a6de75e
soulasphyxia <maslyuk03@bk.ru>
2023-11-13 03:00:57 +0700

    lab 12 commit

---
+++ lab12/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace lab12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private DispatcherTimer timer;
+        private RotateTransform3D rotateY;
+        private TranslateTransform3D translateX;
+        private Transform3DGroup transform;
+        private AxisAngleRotation3D axisYRotation;
+        List<Planet> planets = new List<Planet>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval += new TimeSpan(10000);
+          
+            planets.Add(new Planet(Mercury, 1));
+            planets.Add(new Planet(Venus, 0.8));
+            planets.Add(new Planet(Earth, 0.75));
+            planets.Add(new Planet(Mars, 0.6));
+            planets.Add(new Planet(Jupiter, 0.3));
+            planets.Add(new Planet(Saturn, 0.4));
+            planets.Add(new Planet(Uranus, 0.5));
+            planets.Add(new Planet(Neptune, 0.55));
+
+
+            
+
+            timer.Start();
+
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach(Planet planet in planets)
+            {
+                planet.Rotate();
+            }  
+               
+        }
+
+
+
+        
+    }
+}
---
+++ lab12/Planet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace lab12
+{
+    internal class Planet
+    {
+        private ModelVisual3D _planet;
+        private double angle;
+        private AxisAngleRotation3D axisYRotation;
+        public Planet(ModelVisual3D _planet, double angle)
+        {
+            this.angle = angle;
+            RotateTransform3D rotateY = new RotateTransform3D();
+            axisYRotation = new AxisAngleRotation3D();
+            axisYRotation.Angle = 0;
+            axisYRotation.Axis = new Vector3D(0, 1, 0);
+            rotateY.CenterZ = -10;
+            rotateY.CenterX = 4.5;
+            rotateY.Rotation = axisYRotation;
+            Transform3DGroup transform = new Transform3DGroup();
+            _planet.Transform = transform;
+            transform.Children.Add(rotateY);
+        }
+
+        public ModelVisual3D getPlanet
+        {
+            get { return _planet; }
+
+        }
+        public double getAngle
+        {
+            get
+            {
+                return angle;
+            }
+        }
+
+
+        public void Rotate()
+        {
+            axisYRotation.Angle += angle;
+        }
+
+    }
+}
